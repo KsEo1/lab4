770f0b4ba7ffdb056c906e1cec1a114a41aee097
Paweł Leszczyński <pawelleszczynskiprogrammer@gmail.com>
2022-01-17 15:46:00 +0100

    zad 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        prostokat prostokat = new prostokat();
+        prostokat.nazwaFigury("prostokat");
+        System.out.println(prostokat.obwod(5, 6));
+        System.out.println(prostokat.pole(2, 6));
+
+
+        kwadrat kwadrat = new kwadrat();
+        kwadrat.nazwaFigury("kwadrat");
+        System.out.println(kwadrat.obwod(532, 131));
+        System.out.println(kwadrat.pole(113, 56));
+
+
         /* zad.1
         a) stworzyć klasę abstrakcyjną Figura
         b) stworzyć metody abstrakcyjne pole() i obwod()
